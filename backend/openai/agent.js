ca6760e66285e3ac7b89dc50f6b1d78e5eca4737
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2024-08-13 14:00:49 -0400

    integrated AzureOpenAI (for Azure OpenAI Service deployments) into agent.js

--- backend/openai/agent.js
+++ backend/openai/agent.js
@@ -1,16 +1,32 @@
 import "dotenv/config.js";
-import OpenAI from "openai";
+import { OpenAI, AzureOpenAI } from "openai";
 
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
-});
+// setup OpenAI client (OpenAI or Azure OpenAI)
+// apiVersion required for AzureOpenAI, refer to 
+// For AzureOpenAI, deployment is not a requirement, but you must use the appropriate deployment name as the 'model' property in the endpoint calls
+const apiVersion = "2024-05-01-preview";
+const client = (process.env.AOAI_KEY) ? new AzureOpenAI({ apiKey: process.env.AOAI_KEY, apiVersion: apiVersion, endpoint: process.env.AOAI_ENDPOINT }) : new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+// Deployment (model) names on Azure OpenAI Service (as of openai Version 4.52.7)
+const describingDeploymentName = "omni";
+const imageGenerationDeploymentName = "imageGeneration";
+
+// Model names on OpenAI API (as of openai Version 4.52.7)
+const describeModelName = "gpt-4o";
+const imageModelName = "dall-e-3";
+
+const textModel = (client instanceof AzureOpenAI) ? describingDeploymentName : describeModelName;
+const imageModel = (client instanceof AzureOpenAI) ? imageGenerationDeploymentName : imageModelName;
 
 const describePrompt = `Return a description of this image that can be used to accurately recreate the image. 
                         Do not include references to any style the image might have.
                         Start your description with 'The image depicts'`;
 export async function describeImage(imageURL) {
-    const res = await openai.chat.completions.create({
-        model: "gpt-4o",
+    (client instanceof AzureOpenAI) ?
+        console.log('Azure Deployment called') :
+        console.log("OpenAI API called");
+    const res = await client.chat.completions.create({
+        model: textModel,
         max_tokens: 200,
         messages: [
             {
@@ -32,8 +48,11 @@ export async function describeImage(imageURL) {
 }
 
 export async function transformImage(imagePrompt, imageSize) {
-    const res = await openai.images.generate({
-        model: "dall-e-3",
+    (client instanceof AzureOpenAI) ?
+    console.log('Azure Deployment called') :
+    console.log("OpenAI API called");
+    const res = await client.images.generate({
+        model: imageModel,
         style: "vivid",
         size: imageSize,
         prompt: imagePrompt,
