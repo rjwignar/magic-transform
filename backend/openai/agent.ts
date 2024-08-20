8346d9b47179b7e5360308ecf0716d3f620ae738
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2024-08-20 15:54:25 -0400

    agent.ts TypeScript migration: added approrpriate type declarations to agent.ts where required

--- backend/openai/agent.ts
+++ backend/openai/agent.ts
@@ -1,13 +1,15 @@
 import "dotenv/config.js";
 import { OpenAI, AzureOpenAI } from "openai";
 import logger from "../logger.js"
+import { ImageGenerateParams } from "openai/resources/images.js";
+import { ChatCompletionContentPartImage } from "openai/resources/index.js";
 // setup OpenAI client (OpenAI or Azure OpenAI)
 // apiVersion required for AzureOpenAI, refer to 
 // For AzureOpenAI, deployment is not a requirement, but you must use the appropriate deployment name as the 'model' property in the endpoint calls
 const apiVersion = "2024-05-01-preview";
 const client = (process.env.AOAI_KEY) ? new AzureOpenAI({ apiKey: process.env.AOAI_KEY, apiVersion: apiVersion, endpoint: process.env.AOAI_ENDPOINT }) : new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-function handleOpenAIError(error) {
+function handleOpenAIError(error: Error | any) {
     error.message = `OpenAI API Error: ${error.message}`
     logger.error(error.message);
     throw error;
@@ -31,7 +33,9 @@ const imageModel = (client instanceof AzureOpenAI) ? imageGenerationDeploymentNa
 const describePrompt = `Return a description of this image that can be used to accurately recreate the image. 
                         Do not include references to any style the image might have.
                         Start your description with 'The image depicts'`;
-export async function describeImage(imageURL) {
+
+// ChatCompletionContentPartImage.ImageURL['url'] is `string` type
+export async function describeImage(imageURL: ChatCompletionContentPartImage.ImageURL['url']) {
     whatClient();
     try {
         const res = await client.chat.completions.create({
@@ -59,7 +63,9 @@ export async function describeImage(imageURL) {
     }
 }
 
-export async function transformImage(imagePrompt, imageSize) {
+// ImageGenerateParams['prompt'] is `string` type
+// ImageGenerateParams['size'] is a model-dependent Union type
+export async function transformImage(imagePrompt: ImageGenerateParams['prompt'], imageSize: ImageGenerateParams['size']) {
     whatClient();
     try {
         const res = await client.images.generate({
