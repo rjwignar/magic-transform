0ae037975ec879477f7b483b260cdb3d10a4dff0
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2024-08-19 20:38:43 -0400

    added pino pretty options to format time and ignore pid, hostname

--- backend/logger.js
+++ backend/logger.js
@@ -1,7 +1,11 @@
 import pino from "pino";
 const logger = pino({
     transport: {
-        target: "pino-pretty"
+        target: "pino-pretty",
+        options: {
+            translateTime: "SYS:dd-mm-yyyy HH:MM:ss",
+            ignore: "pid,hostname",
+        }
     }
 });
 
